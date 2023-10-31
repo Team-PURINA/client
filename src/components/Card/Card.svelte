7d80103b61f3348b7ce561059a3a2c9d0d17eda5
이명재 <arkk20000@gmail.com>
2023-10-31 10:39:28 +0900

    feat : card publishing

---
+++ src/components/Card/Card.svelte
@@ -0,0 +1,49 @@
+<script lang="ts">
+	import classnames from 'classnames';
+	import DecreaseArrowIcon from 'src/assets/DecreaseArrowIcon.svelte';
+	import IncreaseArrowIcon from 'src/assets/IncreaseArrowIcon.svelte';
+
+	export let classname: string;
+
+	export let coinName: string;
+
+	export let price: number;
+	export let variancePrice: number;
+	export let varianceRate: number;
+</script>
+
+<div
+	class={classnames(
+		'rounded-2xl w-[26.875rem] h-[16.25rem]  pt-[1.375rem] pb-[2.625rem] px-7 bg-gradient-to-br flex flex-col justify-between',
+		classname
+	)}
+>
+	<div>
+		<p class="text-[#CCC] text-[1.375rem] leading-normal not-italic font-medium">{coinName}</p>
+		<p class="mb-2 text-white text-[1.75rem] leading-normal not-italic font-extrabold">
+			${price.toLocaleString('ru-RU')}
+		</p>
+		<div class="flex items-center">
+			{#if varianceRate > 0}
+				<IncreaseArrowIcon />
+			{:else}
+				<DecreaseArrowIcon />
+			{/if}
+			<p
+				class={classnames(
+					'ml-1 text-xs leading-normal not-italic font-semibold',
+					varianceRate > 0 ? 'text-[#00F919]' : 'text-[#FF5454]'
+				)}
+			>
+				{varianceRate}%
+			</p>
+			<p class="ml-2 text-[#888] text-[0.625rem] leading-normal not-italic font-semibold">
+				{variancePrice}
+				{varianceRate > 0 ? '증가' : '감소'}
+			</p>
+		</div>
+	</div>
+	<p class="text-white text-lg leading-normal not-italic font-normal font-['BCcardB']">
+		9440-3232-1780-2341
+	</p>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,11 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	import Card from 'src/components/Card/Card.svelte';
+</script>
+
+<Card
+	classname="from-[#673AB8] to-[#DBC7FF]"
+	coinName="Preact"
+	price={340_281_000}
+	varianceRate={1.8}
+	variancePrice={80_500_000}
+/>
