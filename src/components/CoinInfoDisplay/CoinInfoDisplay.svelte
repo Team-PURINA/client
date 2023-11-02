7d390b3aa3675ef102be581a9e4d65588d119b2a
이명재 <arkk20000@gmail.com>
2023-11-02 12:30:07 +0900

    feat : Publish wallet info display

---
+++ src/components/CoinInfoDisplay/CoinInfoDisplay.svelte
@@ -0,0 +1,57 @@
+<p class="text-white text-[1.375rem] font-bold leading-normal mb-[2.125rem]">코인 카드</p>
+
+<div class="flex gap-1 items-end">
+	<p class="text-white text-5xl font-bold">SBT</p>
+	<p class="text-[#888] text-2xl font-semibold">Spring Boot</p>
+</div>
+<p class="mb-5">
+	<span
+		style="color: #30BAF0; font-size: 22px; font-family: Pretendard; font-weight: 700; word-wrap: break-word"
+		class="text-[#30BAF0] text-[1.375rem] font-bold break-words"
+	>
+		$693 014 400
+	</span>
+	<span
+		style="color: #888888; font-size: 16px; font-family: Pretendard; font-weight: 700; word-wrap: break-word"
+	>
+		300코인 보유 x 23 100 48
+	</span>
+</p>
+<div class="flex flex-col gap-8">
+	<div>
+		<div class="flex gap-1.5 items-center mb-[0.1875rem]">
+			<p class="text-white">총 수익</p>
+			<div class="flex items-center gap-[0.3125rem]">
+				<p class="text-xl font-bold text-[#0ECB81]">3.8%</p>
+				<p class="text-[#888] font-semibold text-base">$200 288 000</p>
+			</div>
+		</div>
+		<p class="text-[#888] text-xs font-medium">총 몇 퍼센트의 수익을 냈는지를 알려줘요.</p>
+	</div>
+	<div>
+		<div class="flex gap-1.5 items-center mb-[0.1875rem]">
+			<p class="text-white">평균 단가</p>
+			<p class="text-xl font-bold text-[#0ECB81]">$90 210</p>
+		</div>
+		<p class="text-[#888] text-xs font-medium">
+			현재 단가는 $210 900원, 우빈님은 $120 690일 때 구매했어요.
+		</p>
+	</div>
+	<div>
+		<div class="flex gap-1.5 items-center mb-[0.1875rem]">
+			<p class="text-white">총 구매수</p>
+			<p class="text-xl font-bold text-white">3 210</p>
+		</div>
+		<p class="text-[#888] text-xs font-medium">이때까지 구매한 해당 코인의 개수를 보여줘요.</p>
+	</div>
+	<div>
+		<div class="flex gap-1.5 items-center mb-[0.1875rem]">
+			<p class="text-white">마지막 거래일</p>
+			<p class="text-xl font-bold text-white">2023.10.30. 오후 12:58:21</p>
+		</div>
+		<p class="text-[#888] text-xs font-medium">
+			마지막으로 해당 코인을 매도/매수한 날짜를 보여줘요.
+		</p>
+	</div>
+</div>
+<div class="mt-[3.75rem] w-[22.625rem] h-[14.375rem] border-b-[0.0625rem] border-l-[0.0625rem]" />
--- src/routes/wallet/+page.svelte
+++ src/routes/wallet/+page.svelte
@@ -1,4 +1,7 @@
 <script lang="ts">
+	import Card from '@/components/Card/Card.svelte';
+	import CoinInfoDisplay from '@/components/CoinInfoDisplay/CoinInfoDisplay.svelte';
+
 	const WALLETS = [
 		{
 			id: 1,
@@ -75,63 +78,17 @@
 	] as const;
 </script>
 
-<div class="w-full h-full px-11 py-9 flex">
-	<div>
-		<p class="text-white text-[1.375rem] font-bold leading-normal mb-[2.125rem]">코인 카드</p>
-
-		<div class="flex gap-1 items-end">
-			<p class="text-white text-5xl font-bold">SBT</p>
-			<p class="text-[#888] text-2xl font-semibold">Spring Boot</p>
-		</div>
-		<p class="mb-5">
-			<span
-				style="color: #30BAF0; font-size: 22px; font-family: Pretendard; font-weight: 700; word-wrap: break-word"
-				class="text-[#30BAF0] text-[1.375rem] font-bold break-words"
-			>
-				$693 014 400
-			</span>
-			<span
-				style="color: #888888; font-size: 16px; font-family: Pretendard; font-weight: 700; word-wrap: break-word"
-			>
-				300코인 보유 x 23 100 48
-			</span>
-		</p>
-		<div class="flex flex-col gap-8">
-			<div>
-				<div class="flex gap-1.5 items-center mb-[0.1875rem]">
-					<p class="text-white">총 수익</p>
-					<div class="flex items-center gap-[0.3125rem]">
-						<p class="text-xl font-bold text-[#0ECB81]">3.8%</p>
-						<p class="text-[#888] font-semibold text-base">$200 288 000</p>
-					</div>
-				</div>
-				<p class="text-[#888] text-xs font-medium">총 몇 퍼센트의 수익을 냈는지를 알려줘요.</p>
-			</div>
-			<div>
-				<div class="flex gap-1.5 items-center mb-[0.1875rem]">
-					<p class="text-white">평균 단가</p>
-					<p class="text-xl font-bold text-[#0ECB81]">$90 210</p>
-				</div>
-				<p class="text-[#888] text-xs font-medium">
-					현재 단가는 $210 900원, 우빈님은 $120 690일 때 구매했어요.
-				</p>
-			</div>
-			<div>
-				<div class="flex gap-1.5 items-center mb-[0.1875rem]">
-					<p class="text-white">총 구매수</p>
-					<p class="text-xl font-bold text-white">3 210</p>
-				</div>
-				<p class="text-[#888] text-xs font-medium">이때까지 구매한 해당 코인의 개수를 보여줘요.</p>
-			</div>
-			<div>
-				<div class="flex gap-1.5 items-center mb-[0.1875rem]">
-					<p class="text-white">마지막 거래일</p>
-					<p class="text-xl font-bold text-white">2023.10.30. 오후 12:58:21</p>
-				</div>
-				<p class="text-[#888] text-xs font-medium">
-					마지막으로 해당 코인을 매도/매수한 날짜를 보여줘요.
-				</p>
-			</div>
-		</div>
+<div class="w-full h-full px-11 py-9 flex gap-[5.375rem] justify-center">
+	<div class="shrink-0">
+		<CoinInfoDisplay />
+	</div>
+	<div class="w-[30.375rem] h-full shrink-0">
+		<Card
+			classname="from-[#673AB8] to-[#DBC7FF]"
+			coinName="Preact"
+			price={340_281_000}
+			variancePrice={80_500_000}
+			varianceRate={1.9}
+		/>
 	</div>
 </div>
