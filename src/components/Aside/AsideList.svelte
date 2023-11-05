67399e60722150c77246c68a576026bf796571d2
이명재 <arkk20000@gmail.com>
2023-11-06 01:39:27 +0900

    feat : 라우터 푸쉬 추가

--- src/components/Aside/AsideList.svelte
+++ src/components/Aside/AsideList.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+	import { goto } from '$app/navigation';
 	import HomeIcon from '@/assets/HomeIcon.svelte';
 	import NoticeIcon from '@/assets/NoticeIcon.svelte';
 	import TradeIcon from '@/assets/TradeIcon.svelte';
@@ -8,16 +9,25 @@
 	import AsideCoinList from './AsideCoinList.svelte';
 
 	const pages = [
-		{ name: '홈', component: HomeIcon },
-		{ name: '공지사항', component: NoticeIcon },
-		{ name: '거래', component: TradeIcon },
-		{ name: '거래내역', component: TradeRecordIcon },
-		{ name: '지갑', component: WalletIcon }
+		{ name: '홈', icon: HomeIcon },
+		{ name: '공지사항', icon: NoticeIcon },
+		{ name: '거래', icon: TradeIcon },
+		{ name: '거래내역', icon: TradeRecordIcon },
+		{ name: '지갑', icon: WalletIcon }
 	];
+
+	const pagesMap: Record<string, string> = {
+		홈: '/',
+		공지사항: '/notice',
+		거래: '/trade',
+		거래내역: '/trade/history',
+		지갑: '/wallet'
+	};
 
 	let currentPage = '홈';
 
 	const handlePageButtonClick = (pagename: string) => {
+		goto(pagesMap[pagename]);
 		currentPage = pagename;
 	};
 </script>
@@ -32,11 +42,11 @@
 				on:click={() => handlePageButtonClick(page.name)}
 				id={page.name}
 				class={classNames(
-					' hover:bg-[#242424] w-52 flex items-center px-5 py-4 gap-4 rounded-md',
-					page.name === currentPage && 'bg-[#2C2C2C]'
+					'w-52 flex items-center px-5 py-4 gap-4 rounded-md',
+					page.name === currentPage ? 'bg-[#2C2C2C]' : 'hover:bg-[#242424]'
 				)}
 			>
-				<svelte:component this={page.component} />
+				<svelte:component this={page.icon} />
 				<span class=" text-[#018FC6] text-sm font-semibold">{page.name}</span>
 			</button>
 		{/each}
