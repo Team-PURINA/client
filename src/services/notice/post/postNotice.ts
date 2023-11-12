c0f03609eb88b0b8fe6a6c463590933e78624faa
이명재 <arkk20000@gmail.com>
2023-11-13 08:35:35 +0900

    feat : 리뷰 반영

--- src/services/notice/post/postNotice.ts
+++ src/services/notice/post/postNotice.ts
@@ -1,7 +1,7 @@
 import { axiosInstance } from '@/apis';
 
-const postNotice = async ({ title, content }: { title: string; content: string }) => {
-	const { data } = await axiosInstance.post('/notice', { title, content });
+const postNotice = async (postData: { title: string; content: string }) => {
+	const { data } = await axiosInstance.post('/notice', postData);
 	return data;
 };
 
